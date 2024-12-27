d496a32ddd542b3218ee926975193c30704dffde
kernelpanic95 <marcocardone29@gmail.com>
2024-12-27 23:29:12 +0100

    done

--- index.js
+++ index.js
@@ -102,37 +102,7 @@ function updateToggleModeBtn(){
 }
 
 
-const promptWindow =  new Prompt("#pixa-playground")
-const promptForm = document.querySelector("#prompt-form")
-const promptInput = promptForm.querySelector("input[name='prompt']")
 
-const MAX_PROMPTS = 3
-
-promptForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-
-    // window.open("https://github.com/PaulleDemon", "_blank")
-
-    if (promptWindow.promptList.length >= MAX_PROMPTS)
-        return false
-
-    promptWindow.addPrompt(promptInput.value)
-    promptInput.value = ""
-    
-    if (promptWindow.promptList.length >= MAX_PROMPTS){
-        // prompt signup once the user makes 3 prompts, ideally must be throttled via backend API
-        const signUpPrompt = document.querySelector("#signup-prompt")
-        signUpPrompt.classList.add("tw-scale-100")
-        signUpPrompt.classList.remove("tw-scale-0")
-
-        promptForm.querySelectorAll("input").forEach(e => {e.disabled = true})
-    }
-
-    return false
-})
-
-const dropdowns = document.querySelectorAll('.dropdown')
-dropdowns.forEach(dropdown => new Dropdown(`#${dropdown.id}`, promptWindow.setAIModel))
 
 
 navToggle.addEventListener("click", toggleNavDropdown)
@@ -174,99 +144,6 @@ function closeNavDropdown(event){
     navDropdown.setAttribute("data-open", false)
 
 }
-
-
-const videoBg = document.querySelector("#video-container-bg")
-const videoContainer = document.querySelector("#video-container")
-
-function openVideo(){
-    videoBg.classList.remove("tw-scale-0", "tw-opacity-0")
-    videoBg.classList.add("tw-scale-100", "tw-opacity-100")
-    videoContainer.classList.remove("tw-scale-0")
-    videoContainer.classList.add("tw-scale-100")
-
-    document.body.classList.add("modal-open")
-}
-
-function closeVideo(){
-    videoContainer.classList.add("tw-scale-0")
-    videoContainer.classList.remove("tw-scale-100")
-
-    setTimeout(() => {
-        videoBg.classList.remove("tw-scale-100", "tw-opacity-100")
-        videoBg.classList.add("tw-scale-0", "tw-opacity-0")
-    }, 400)
-   
-
-    document.body.classList.remove("modal-open")
-
-}
-
-/**
- * Animations
- */
-
-const typed = new Typed('#prompts-sample', {
-    strings: ["How to solve a rubik's cube? Step by step guide", 
-                "What's Pixa playground?", 
-                "How to build an AI SaaS App?", 
-                "How to integrate Pixa API?"],
-    typeSpeed: 80,
-    smartBackspace: true, 
-    loop: true,
-    backDelay: 2000,
-})
-
-gsap.registerPlugin(ScrollTrigger)
-
-
-gsap.to(".reveal-up", {
-    opacity: 0,
-    y: "100%",
-})
-
-
-// straightens the slanting image
-gsap.to("#dashboard", {
-
-    scale: 1,
-    translateY: 0,
-    // translateY: "0%",
-    rotateX: "0deg",
-    scrollTrigger: {
-        trigger: "#hero-section",
-        start: window.innerWidth > RESPONSIVE_WIDTH ? "top 95%" : "top 70%",
-        end: "bottom bottom",
-        scrub: 1,
-        // markers: true,
-    }
-
-})
-
-const faqAccordion = document.querySelectorAll('.faq-accordion')
-
-faqAccordion.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        this.classList.toggle('active')
-
-        // Toggle 'rotate' class to rotate the arrow
-        let content = this.nextElementSibling
-        let icon = this.querySelector(".bi-plus")
-
-        // content.classList.toggle('!tw-hidden')
-        if (content.style.maxHeight === '240px') {
-            content.style.maxHeight = '0px'
-            content.style.padding = '0px 18px'
-            icon.style.transform = "rotate(0deg)"
-            
-        } else {
-            content.style.maxHeight = '240px'
-            content.style.padding = '20px 18px'
-            icon.style.transform = "rotate(45deg)"
-        }
-    })
-})
-
 
 
 // ------------- reveal section animations ---------------
